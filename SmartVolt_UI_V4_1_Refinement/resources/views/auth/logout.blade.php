c4d24ba0678c8bc5669e3a01921ff20008f58274
VICTRYSIUS_LEOALVARNO_KAYAN <vinoallvarnokayan@gmail.com>
2026-08-18 11:33:57 +0700

    Remove obsolete backup and development packages

--- SmartVolt_UI_V4_1_Refinement/resources/views/auth/logout.blade.php
+++
@@ -1,7 +0,0 @@
-<form action="{{ route('logout') }}" method="POST" data-loading-form>
-    @csrf
-    <button type="submit" class="sv-button sv-button-ghost sv-button-sm" data-loading-text="Keluar...">
-        <x-icon name="logout" :size="16" />
-        <span data-button-label>Keluar</span>
-    </button>
-</form>
--- SmartVolt_UI_V6_Streamlined/install-smartvolt-ui-v6.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0install-smartvolt-ui-v6.ps1" -ProjectRoot "C:\SMARTVOLT"
-echo.
-pause
